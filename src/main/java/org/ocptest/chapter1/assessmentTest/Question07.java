20a96f05a3626fb82a78e37e73aca5f0e9c65dba
mauricio-arcanjo <140445426+mauricio-arcanjo@users.noreply.github.com>
2025-06-06 15:04:44 -0400

    Studing chapter 1 - part 1

--- src/main/java/org/ocptest/chapter1/assessmentTest/Question07.java
+++ src/main/java/org/ocptest/chapter1/assessmentTest/Question07.java
@@ -1,4 +1,23 @@
 package org.ocptest.chapter1.assessmentTest;
 
+import java.util.Date;
+import java.sql.*;
+import java.util.Arrays;
+
 public class Question07 {
+    public static void main(String[] args) {
+        int[] array = {6, 9, 8};
+
+        System.out.println("B" + Arrays.binarySearch(array, 9));
+        System.out.println("C" + Arrays.compare(array, new int[] {6, 9, 8}));
+        System.out.println("M" + Arrays.mismatch(array, new int[] {6, 9, 8}));
+        String[] s;
+        String d[];
+
+        java.sql.Date e;
+        Date as;
+
+        java.util.Date date;
+        java.sql.Date date1;
+    }
 }
